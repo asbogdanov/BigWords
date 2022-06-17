03ff8daf11cad4fd7f15f33bb5f26b0631c47e23
Aleksandr Bogdanov <basik0204@gmail.com>
2022-06-17 16:05:03 +0300

    Initial Commit

---
+++ BigWords/BigWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigWordsApp.swift
+//  BigWords
+//
+//  Created by Aleksandr Bogdanov on 17.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct BigWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
